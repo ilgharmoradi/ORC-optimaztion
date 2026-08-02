6979d3f80d0aa99ffafb8089c65be07fd6eae157
ilghar moradi <ilgharmoradi@gmail.com>
2026-08-02 03:45:10 +0330

    fixed thermo mistake - added normalization and future csv support

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
-from utilities import get_temperature , available_fluids
+from utilities import get_temperature , available_fluids , normalization
 from CoolProp.CoolProp import PropsSI
 import CoolProp
+import CoolProp.Plots
 import numpy as np
 from pymoo.core.problem import ElementwiseProblem
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.optimize import minimize
 
-print(CoolProp.__version__)
+import csv
+import time
+
+print("coolprop version:" , CoolProp.__version__)
+run_name = "run with T0 = 40"
 T0 = 20  #default ambient temperature
 T_source = 250
-#available_fluids = available_fluids()
+# available_fluids = available_fluids()
 
-def calculate_thermodynamics(fluids , T_source : float ,P0 : float, P_super_heat : float  , mass_flow_rate : float = 1):
+
+def calculate_thermodynamics(fluids , T_source : float , P_super_heat : float  , mass_flow_rate : float = 1):
     global T0
     fluid,comp = list(fluids.keys()) , list(fluids.values())
     fluid_string = "&".join(fluid)
 
-    state = CoolProp.AbstractState("HEOS",fluid_string)
+    state = CoolProp.AbstractState("SRK",fluid_string)
     state.set_mole_fractions(comp)
-    state.build_phase_envelope("")
+    #state.build_phase_envelope("")
 
+    T_cond = T0 + 15 #small temperature difference for air cooling
+    state.update(CoolProp.QT_INPUTS , 0 , T_cond + 273.15)
+    P0 = state.p()
     # print(fluid_string)
 
     #state [1] after condenser    
-    state.update(CoolProp.QT_INPUTS , 0 , T0 + 273.15 )
+    state.update(CoolProp.PQ_INPUTS , P0 , 0 )
     h1 = state.hmass()
     s2 = state.smass()
 
@@ -46,6 +55,7 @@ def calculate_thermodynamics(fluids , T_source : float ,P0 : float, P_super_heat
     eta = w_net / q_in
     return ( w_net * mass_flow_rate , eta , q_out)
 
+n = 0
 
 class OptimizeProblem(ElementwiseProblem):
     def __init__(self):
@@ -53,7 +63,7 @@ class OptimizeProblem(ElementwiseProblem):
                           xl = np.array([0 , 0 , 0]),
                           xu = np.array([1 , 1 , 1]))
         """
-         take P_source and P0 as given 
+         take P_source as design value or parameter to optimize
          assumed isentropic turbine and pump
          assumed no heat lose in cycle
          assumed no pressure drop in cycle
@@ -61,14 +71,15 @@ class OptimizeProblem(ElementwiseProblem):
          maximize cycle efficiency
         """
     def _evaluate(self, x, out, *args, **kwargs):
-        global T_source
+        global T_source , n
         # TODO: add fluid sampling here
         # TODO: for each goal make separate file  
-        normalized_x = x / np.sum(x)
-        print(normalized_x)
+        normalized_x = normalization(x)
+        # print(n , normalized_x)
         fluids = {"R134a":normalized_x[0],"R32":normalized_x[1],"R125":normalized_x[2]}
+        n += 1
         try:
-            props = calculate_thermodynamics(fluids,T_source, 10e3 , 2e6)
+            props = calculate_thermodynamics(fluids,T_source, 6e6)
 
             g1 = -props[0]
             g2 = -props[1]
@@ -84,13 +95,30 @@ class OptimizeProblem(ElementwiseProblem):
 
 def main():
     global T0
-    temperatures_at_Isfahan = get_temperature(51.6804 , 32.6613 , 2025 , (6 ,7 ,8))
-    print("temperature of Isfahan city during summer:" , temperatures_at_Isfahan)
-    T0 = np.sum(temperatures_at_Isfahan) / 3
-    algorithm = NSGA2(pop_size=80)
+    temperatures_at_Isfahan,error = get_temperature(51.6804 , 32.6613 , 2025 , (6 ,7 ,8))
+    if error != 0:
+        T0 = 40
+    else:
+        print("temperature of Isfahan city during summer:" , temperatures_at_Isfahan)
+        T0 = np.sum(temperatures_at_Isfahan) / 3
+    algorithm = NSGA2(pop_size=100)
     p = OptimizeProblem()
-    res = minimize(p , algorithm , ("n_gen",100) , seed = 1)
-    print(res.X)
-    
+    with open( f"results/{int(time.time())}.csv" if run_name.strip() == "" else f"results/{run_name}.csv" , "w") as f:
+        csv_handler = csv.writer(f)
+        csv_handler.writerow(["run N" , "R134a" , "R32" , "R125" , "eta"])
+
+        t_start = time.time()
+        for i in range(0 , 10):
+            res = minimize(p , algorithm , ("n_gen",80) , seed = i)
+            normalized_x = normalization (res.X)
+            print(normalized_x)
+            # if all(res.X): # type: ignore
+            #     csv_handler.writerow([i , normalized_x[0] , normalized_x[1] , normalized_x[2] , -res.F[0]]) # type: ignore
+            # else:
+            #     csv_handler.writerow([i , 0 , 0 , 0 ,0 ]) 
+            # print(res.X)
+        t_end = time.time()
+        print("calculation time : " , t_end - t_start)
+
 if __name__ == '__main__':
     main()
--- utilities.py
+++ utilities.py
@@ -2,7 +2,7 @@ import requests
 import numpy as np
 from CoolProp.CoolProp import PropsSI
 
-def get_temperature( long , lat ,year,months) :
+def get_temperature( long , lat ,year,months):
      try:
         append = ""
         if isinstance(year , tuple):
@@ -12,10 +12,10 @@ def get_temperature( long , lat ,year,months) :
 
         d = requests.get(f"https://power.larc.nasa.gov/api/temporal/monthly/point?parameters=T2M_MAX&community=SB&longitude={long}&latitude={lat}&format=JSON" + append)
 
-        return np.array(list(d.json()["properties"]["parameter"]["T2M_MAX"].values()))[np.array(months)-1]
+        return (np.array(list(d.json()["properties"]["parameter"]["T2M_MAX"].values()))[np.array(months)-1] , 0)
      except:
          print("error in get request")
-         return 0
+         return (None , 1)
 
 def available_fluids():  
     ORC_FLUIDS = [
@@ -65,13 +65,24 @@ def available_fluids():
         "Novec649"
     ]
 
-    available_fluids = set() 
+    available_fluids = [] 
     for i in ORC_FLUIDS:
         for j in range(ORC_FLUIDS.index(i) + 1 , len(ORC_FLUIDS)):
-            mixture = f"HEOS::{i}[0.5]&{ORC_FLUIDS[j]}[0.5]"
+            mixture = f"SRK::{i}[0.5]&{ORC_FLUIDS[j]}[0.5]"
             try:
                 PropsSI("D", "T", 300, "P", 101325, mixture)
-                available_fluids.add(set([i , ORC_FLUIDS[j]]))
+                available_fluids.append(set((i , ORC_FLUIDS[j])))
             except:
                 pass
     return available_fluids
+
+def fil(a):
+    zero_limit = 1e-5
+    b = a / a.sum()
+    filtered_list =  np.array(list(map(lambda f: 0 if f < 1e-6 else f , b)))
+    return filtered_list / sum(filtered_list)
+def normalization(x):
+    if x.ndim == 1:
+        return fil(x)
+    else:    
+        return np.apply_along_axis(fil, 1 , x)
